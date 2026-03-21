6505b6d8b78faa6d73567276172d9e4784e21cb2
vladislav zilber <vladislavzilber@Mac.ht.home>
2026-03-20 21:11:53 -0400

    Remove duplicate create_db route from app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 from app import create_app
-from app.extensions import db
 
 app = create_app()
-
-
-@app.route("/create-db")
-def create_db():
-    db.create_all()
-    return "Database tables created"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
