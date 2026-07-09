b0a88630e0e9ec8450b21ea0b92c8365df635299
zilbervlad <vlad@bostonpie.com>
2026-07-09 17:14:12 -0400

    Fix photo attachment file reading

--- mobile/src/screens/MessagesScreen.js
+++ mobile/src/screens/MessagesScreen.js
@@ -14,7 +14,7 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import * as ImagePicker from "expo-image-picker";
 
 import {
